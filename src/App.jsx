8ab2f525f8b4bf02c5bc03695d0860e9640a2842
enrico brasil <enrico.brasil@prit.app>
2025-03-29 19:03:15 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { Fragment } from "react";
+
+export default function App() {
+
+  return (
+    <Fragment>
+    <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
+    </Fragment>
+  )
+}
+
+
