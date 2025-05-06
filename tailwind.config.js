c1ac8dada210bb8d854bd4682a03d45bc1e929a0
enrico brasil <enrico.brasil@prit.app>
2025-05-06 19:15:23 -0300

    fit: section tecnologies

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,11 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        customBg: '#e6e6e4',
+      },
+    },
   },
   plugins: [],
 }
